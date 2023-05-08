3f92edbf7e553f56ae9e14199bb8751b9220bbf6
Lê Hữu Uyn <75217659+uyn159@users.noreply.github.com>
2023-05-08 16:35:04 +0700

    config: router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; //npm i react-router-dom
+import { publicRoutes } from '~/routes';
 
 function App() {
     return (
-        <div className="App">
-            <h1>Tiktik app</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        return <Route key={index} path={route.path} element={<Page />} />;
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
